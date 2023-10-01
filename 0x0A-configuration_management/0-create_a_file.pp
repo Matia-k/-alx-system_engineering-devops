b85d18f650cc239687c25f52ce0741a7b1ef0fa2
Matia-k <matia40k@gmail.com>
2023-10-01 10:24:17 -0700

    all project tasks

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,10 @@
+#file created using puppet in /tmp
+
+file { 'school':
+  ensure  =>  'present',
+  content =>  'I love Puppet',
+  group   =>  'www-data',
+  mode    =>  '0744',
+  owner   =>  'www-data',
+  path    =>  '/tmp/school',
+}
